e59aa1519c4d9cc8ee1159951643e6d5f7568354
Mojill-coder <modungwojill2@gmail.com>
2025-05-19 11:18:23 +0100

    Assessment_Q1

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,4 +1,3 @@
--- Q1: High-Value Customers with Multiple Products  -----
 SELECT uc.id AS owner_id,uc.name,COUNT(DISTINCT s.id) AS savings_count,COUNT(DISTINCT p.id) AS investment_count,ROUND(SUM(s.confirmed_amount) / 100.0, 2) AS total_deposits
 FROM users_customuser uc
 JOIN savings_savingsaccount s ON s.owner_id = uc.id AND s.confirmed_amount > 0
